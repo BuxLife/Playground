457487a546f985af6227d96fc31cd1673de900c1
BuxLife <Naeembux@live.com>
2021-11-22 00:50:00 +0200

    Test Templates added TODO: test body.

---
+++ tests.java
@@ -0,0 +1,49 @@
+import java.beans.Transient;
+
+public class tests {
+    
+    public static void main(String[] args) {
+
+    }
+
+    @Test 
+    void zeroIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void negativeIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void singleIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void doubleIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void finiteIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void belowMaxIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void aboveMaxIterationTest(){
+        // TODO
+    }
+
+    @Test 
+    void nestedLoopTest(){
+        // TODO
+    }
+
+}
